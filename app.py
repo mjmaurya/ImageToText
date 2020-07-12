46e359956a16112850fd1f43dfd2dcdcb2883c17
Manoj Kumar <gcssm1999@gmail.com>
2020-07-12 20:41:16 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import render_template
+import sys
+from flask import request,redirect
+import os
+from ocr_core import ocr_core
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER='/static/uploads/'
+app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
+ALLOWED_EXTENTION=set(['png','jpg','jpeg'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENTION
+@app.route("/",methods=['GET','POST'])
+def index():
+    if request.method=='POST':
+        if 'file' not in request.files:
+            return render_template('index.html',msg="No file selected")
+        xfile=request.files['file']
+        if xfile.filename=='':
+            return render_template("index.html",msg='No file selected')
+        print(allowed_file(xfile.filename))
+        if xfile and allowed_file(xfile.filename):
+            print(allowed_file(xfile.filename))
+            extracted_text=ocr_core(xfile)
+            print("manoj    ji")
+            image_src=os.path.join(app.config['UPLOAD_FOLDER'], xfile.filename)
+            return render_template('index.html',msg="Successfully processed",
+                                    extracted_text=extracted_text,
+                                    image_src=image_src)
+        else:
+            return render_template("index.html",msg="Choose Valid File")
+
+    
+    else:
+        return render_template("index.html")
+
+if __name__=='__main__':
+    app.run()
---
+++ ocr_core.py
@@ -0,0 +1,29 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+from PIL import ImageEnhance
+import pytesseract
+from io import BytesIO
+pytesseract.pytesseract.tesseract_cmd=r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+def ocr_core(filename):
+    """
+    This function will handle the core OCR processing of images.
+    """
+    print(filename)
+    print(str(filename))
+    xyz=str(filename).split(" ")[1].split(".")
+    ima=Image.open(filename)
+    image=Image.open(filename)
+    print(xyz[len(xyz)-1].replace("'",""))
+    if xyz[len(xyz)-1].replace("'","")=="png":
+        print("MAnoj sir")
+        with BytesIO() as f:
+            ima.save(f, format='JPEG')
+            ima_jpg = ima.convert('RGB')
+            image = Image.open(ima_jpg)
+    try:
+        text = pytesseract.image_to_string(image,lang="eng+hin") 
+        return text
+    except Exception as ex:
+        return ex
